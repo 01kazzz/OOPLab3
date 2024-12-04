bdb44b8137d2032c1741cbbc02a22c25e3ab1ccc
01kazzz <kopach.karina@gmail.com>
2024-12-04 14:21:24 +0200

    Add files via upload

---
+++ OOPlab_3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Введіть кількість рядків (n): ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Введіть кількість стовпців (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        double[,] array = new double[n, m];
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                array[i, j] = Math.Round(rand.NextDouble() * (7.03 - (-42.31)) + (-42.31), 2);
+            }
+        }
+
+        Console.WriteLine("Початковий масив:");
+        PrintArray(array);
+
+        int nonNegativeRowsCount = 0;
+        for (int i = 0; i < n; i++)
+        {
+            bool hasNegative = false;
+            for (int j = 0; j < m; j++)
+            {
+                if (array[i, j] < 0)
+                {
+                    hasNegative = true;
+                    break;
+                }
+            }
+            if (!hasNegative)
+            {
+                nonNegativeRowsCount++;
+            }
+        }
+        Console.WriteLine($"Кількість рядків, які не містять жодного від’ємного елемента: {nonNegativeRowsCount}");
+
+        for (int j = 0; j < m; j++)
+        {
+            for (int i = 0; i < n / 2; i++)
+            {
+                double temp = array[i, j];
+                array[i, j] = array[n - 1 - i, j];
+                array[n - 1 - i, j] = temp;
+            }
+        }
+
+        Console.WriteLine("Масив після зміни порядку елементів у стовпцях:");
+        PrintArray(array);
+    }
+
+    public static void PrintArray(double[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"{array[i, j]:F2}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ OOPlab_3/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ArrayApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateArray_Click(object sender, RoutedEventArgs e)
+        {
+            int n, m;
+            if (int.TryParse(RowsInput.Text, out n) && int.TryParse(ColsInput.Text, out m))
+            {
+                double[,] array = new double[n, m];
+                Random rand = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        array[i, j] = Math.Round(rand.NextDouble() * (7.03 - (-42.31)) + (-42.31), 2);
+                    }
+                }
+
+                ArrayDisplayPanel.Children.Clear();
+                DisplayArray(array, "Початковий масив:");
+
+                int nonNegativeRowsCount = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    bool hasNegative = false;
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (array[i, j] < 0)
+                        {
+                            hasNegative = true;
+                            break;
+                        }
+                    }
+                    if (!hasNegative)
+                    {
+                        nonNegativeRowsCount++;
+                    }
+                }
+                ResultText.Text = $"Кількість рядків, які не містять жодного від’ємного елемента: {nonNegativeRowsCount}";
+
+                for (int j = 0; j < m; j++)
+                {
+                    for (int i = 0; i < n / 2; i++)
+                    {
+                        double temp = array[i, j];
+                        array[i, j] = array[n - 1 - i, j];
+                        array[n - 1 - i, j] = temp;
+                    }
+                }
+                DisplayArray(array, "Масив після зміни порядку елементів у стовпцях:");
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, введіть дійсні числа для кількості рядків і стовпців.");
+            }
+        }
+
+        private void DisplayArray(double[,] array, string title)
+        {
+            var textBlock = new TextBlock
+            {
+                Text = title,
+                Margin = new Thickness(0, 10, 0, 5),
+                FontWeight = FontWeights.Bold
+            };
+            ArrayDisplayPanel.Children.Add(textBlock);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                string row = "";
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    row += $"{array[i, j]:F2}\t";
+                }
+                ArrayDisplayPanel.Children.Add(new TextBlock { Text = row });
+            }
+        }
+    }
+}
